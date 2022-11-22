2d7465553c3d51188c832d9212301428645ba0db
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2022-11-22 13:36:00 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((request, response)=>{
+  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+  response.writeHead(200,{'Content-type':'text/html'});
+  response.end('<h1>Hello world!</h1>');
+})
+
+server.listen(3000, () => console.log('Server started at http://localhost:3000'));
