ffd7fa982a87c2f7585a48c610249b8d8238d3fa
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2022-11-22 13:46:34 -0300

    feat: add users mocks

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const http = require('http');
 
+const users = require('./mocks/users');
+
 const server = http.createServer((request, response)=>{
   console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
-  response.writeHead(200,{'Content-type':'text/html'});
-  response.end('<h1>Hello world!</h1>');
+
+  if(request.url === '/users' && request.method === 'GET'){
+    response.writeHead(200,{'Content-type':'application/json'});
+    response.end(JSON.stringify(users));
+  }
+
+  // response.writeHead(200,{'Content-type':'text/html'});
+  // response.end('<h1>Hello world!</h1>');
 })
 
 server.listen(3000, () => console.log('Server started at http://localhost:3000'));
